f758d1cc1c7cec403988e4fb06e2e19170868c35
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-05 23:39:59 +0300

    create a refistration form

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,17 @@
 from flask import render_template
-from app import app
+from app import main
+from flask_login import login_required
 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+    return render_template('')
--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from . import db
+from flask_login import UserMixin
+from . import login_manager
 
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),unique=True,nullable=False)
@@ -23,3 +26,8 @@ class Pitch(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
     def __repr__(self):
         return f'Pitch {self.title}'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
