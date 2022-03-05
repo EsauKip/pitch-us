de227e7c1609e17b15fb4eb9bfb2abe45aff26dd
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-05 22:04:05 +0300

    update the user model

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,5 +2,17 @@ from flask import Flask
 
 # Initializing application
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] ='iamkip'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://moringa:kiprono12@localhost/pitches'
+pitch=[
+    {
+        'author':'haveys',
+        'title':'first post',
+        'content': 'be careful!'   },
+        {
+            'author':'jim reeves',
+            'title':'second post',
+            'content': 'never ever give up'
+        }
+]
 from app import views
--- app/models.py
+++ app/models.py
@@ -4,10 +4,22 @@ from . import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-
+    username = db.Column(db.String(255),unique=True,nullable=False)
+    email = db.Column(db.String(255),unique = True,nullable=False) 
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpeg')
+    password = db.Column(db.String(60),nullable=False)
+    pitches_id =db.Column(db.Integer,db.ForeignKey('pitche.id'))
+    
     def __repr__(self):
-        return f'User {self.username}'
-class pitch(db.models.Model):
+        return f'User {self.username},{self.email},{self.image_file}'
+class Pitch(db.Model):
     __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.String)
+    users = db.relationship('User',backref = 'pitches',lazy="dynamic")
+
+    # category = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    def __repr__(self):
+        return f'User {self.name}'
