caa95793c61ceb2a253420486b998429c78a57c2
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-06 00:22:28 +0300

    add the lig out required function

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
+
 from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
@@ -28,3 +29,8 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)    
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
