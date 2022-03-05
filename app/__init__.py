07506a0b547a388d2f0cb6c9c36ed7948e5efb30
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-05 22:30:39 +0300

    create auth file,initialize the files blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -10,19 +10,8 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # ....
-        # Initializing flask extensions
-  
-    # if (
-    #         'SQLALCHEMY_DATABASE_URI' not in app.config and
-    #         'SQLALCHEMY_BINDS' not in app.config
-    #     ):
-    #         warnings.warn(
-    #             'Neither SQLALCHEMY_DATABASE_URI nor SQLALCHEMY_BINDS is set. '
-    #             'Defaulting SQLALCHEMY_DATABASE_URI to "sqlite:///:memory:".'
-    #         )
-    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     app.config.from_object(config_options[config_name])
     # Initializing flask extensions
--- app/models.py
+++ app/models.py
@@ -8,18 +8,18 @@ class User(db.Model):
     email = db.Column(db.String(255),unique = True,nullable=False) 
     image_file = db.Column(db.String(20),nullable=False,default='default.jpeg')
     password = db.Column(db.String(60),nullable=False)
-    pitches_id =db.Column(db.Integer,db.ForeignKey('pitche.id'))
+    pitches=db.relationship('Pitch',backref='author',lazy=True)
     
     def __repr__(self):
         return f'User {self.username},{self.email},{self.image_file}'
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(255))
-    content = db.Column(db.String)
-    users = db.relationship('User',backref = 'pitches',lazy="dynamic")
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.text,nullable=False)
+    # users = db.relationship('User',backref = 'pitches',lazy="dynamic")
 
     # category = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
     def __repr__(self):
-        return f'User {self.name}'
+        return f'Pitch {self.title}'
