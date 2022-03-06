41dca9ac452be35be4085ec007316818faf004e3
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-06 13:57:33 +0300

    add pitch form

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='iamkip'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://moringa:kiprono12@localhost/pitches'
+
 pitch=[
     {
         'author':'haveys',
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import main
-from flask_login import login_required
+from flask_login import login_required,redirect,url_for,current_user
+from ..models import User, Pitch, Category
+from .forms import PitchForm
 
 
 # Views
@@ -10,8 +12,23 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+    Category=Category.query_all()
+    title='Pitch'
+    return render_template('index.html',title=title,Category=Category)
 @login_required
 def new_pitch():
-    return render_template('')
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        category = form.category.data
+        newPitch = form.pitch_info.data
+        #update pitch instance
+        new_pitch = Pitch(pitch_title=title,
+                          pitch_category=category,
+                          pitch_itself=newPitch,
+                          user=current_user)
+        #save pitch
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+    title = 'Add New pitch'
+    return render_template('pitches.html', title=title, pitchesform=form)
