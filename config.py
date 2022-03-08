8706a3a27ccaa587d31d991d76569ea8c9bb5a56
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-08 13:50:44 +0300

    deployment2

--- config.py
+++ config.py
@@ -17,10 +17,10 @@ class config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-class prodConfig(config):
+class prodConfig():
     """ production configuration class child class
     """    
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgres://dqymjvexvlkdnu:cd96a74c6402bad8fb1fa0c96de4a93782fcc65c293f6f20fa9f0fd958547f4c@ec2-54-209-221-231.compute-1.amazonaws.com:5432/de7rsdpmpu2c42'
 class devConfig(config):
     """ dev configuration class child class
     """
